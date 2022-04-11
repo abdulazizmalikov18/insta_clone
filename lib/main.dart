14c7b155e586fa5e1bbd197f23430f9b68938048
abdulazizmalikov18 <abdulazizmalikov98@gmail.com>
2022-04-11 17:32:58 +0500

    FireBase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:insta_clone/responsive/mobile_screen_layout.dart';
 import 'package:insta_clone/responsive/responsive_leyout_screen.dart';
 import 'package:insta_clone/responsive/web_screen_layout.dart';
 import 'package:insta_clone/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options:const FirebaseOptions(
+        apiKey: "AIzaSyCpJYhunLOHTv4NbcCENZpCHn-z6-j4tSs",
+        appId: "1:824131335431:web:337679035ba95a313e81a2",
+        messagingSenderId: "824131335431",
+        projectId: "insta-clone-f030d",
+        storageBucket: "insta-clone-f030d.appspot.com",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
